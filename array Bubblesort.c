b5bb017e0ab15cd2631c5c314163f0480b763536
hema-2008 <112534078@kanchiuniv.ac.in>
2026-02-19 20:41:28 -0800

    Add files via upload

---
+++ array Bubblesort.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,j,temp;
+	printf("enter number of elments:");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("enter %d elements:\n",n);
+	for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	
+	//bubble sort logic
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-1;j++)
+	{
+	    if(arr[j]>arr[j+1])
+	    {
+		  temp = arr[j];
+		  arr[j] = arr[j+1];
+		  arr[j+1]=temp;
+	}
+    }
+    }
+    printf("sorted array:\n");
+    for(i=0;i<n;i++)
+    printf("%d",arr[i]);
+    
+    return 0;
+}
